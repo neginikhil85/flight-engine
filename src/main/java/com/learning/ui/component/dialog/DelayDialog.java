5dd39eed1ebb40a2737d4e72d3eed8406aad320b
Nikhil Negi <neginikhil85@gmail.com>
2024-08-02 10:56:48 +0530

    close button added on dialog box

--- src/main/java/com/learning/ui/component/dialog/DelayDialog.java
+++ src/main/java/com/learning/ui/component/dialog/DelayDialog.java
@@ -3,8 +3,11 @@ package com.learning.ui.component.dialog;
 import com.learning.ui.component.grid.CustomGrid;
 import com.learning.ui.component.grid.provider.ColumnProviderFactory;
 import com.learning.ui.model.DelayData;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dialog.Dialog;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 
 import java.util.List;
 
@@ -13,7 +16,20 @@ public class DelayDialog extends Dialog {
     CustomGrid<DelayData.DelayItem> nestedGrid;
 
     public DelayDialog(List<DelayData.DelayItem> delayItems, ColumnProviderFactory factory) {
-        setHeaderTitle("Delays");
+        setDraggable(true);
+
+        Div headerTitle = new Div();
+        headerTitle.setText("Delays");
+        headerTitle.addClassName("dialog-title");
+
+        Button closeBtn = new Button();
+        closeBtn.addClickListener(click -> this.close());
+        closeBtn.addClassName("dialog-close-btn");
+        closeBtn.setIcon(VaadinIcon.CLOSE.create());
+
+        HorizontalLayout headerLayout = new HorizontalLayout(headerTitle, closeBtn);
+        headerLayout.addClassName("dialog-header");
+        headerLayout.setWidthFull();
 
         // Create a nested grid to show delay items
         nestedGrid = new CustomGrid<>(DelayData.DelayItem.class, factory);
@@ -21,6 +37,6 @@ public class DelayDialog extends Dialog {
         nestedGrid.setItems(delayItems);
 
         // Add the grid to the dialog
-        add(nestedGrid);
+        add(headerLayout, nestedGrid);
     }
 }
